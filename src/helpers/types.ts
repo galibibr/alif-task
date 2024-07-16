05c17cfa2ff0298767d9467dd5cfe1f88c2d75b6
GalibIbr <galibiavi4@gmail.com>
2024-07-16 22:30:15 +0300

    Add types for User

---
+++ src/helpers/types.ts
@@ -0,0 +1,13 @@
+export interface User {
+   id: number
+   sex: 'male' | 'female'
+   fullname: string
+   email: string
+   phone: string
+   birthday: string
+   languages: string[]
+   major: string
+   skills: string[]
+   experience: number
+   about: string
+}
