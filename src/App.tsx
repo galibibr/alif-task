05e321edcd762508acb4bd073c12b0042c212555
GalibIbr <galibiavi4@gmail.com>
2024-07-16 22:55:21 +0300

    Add store

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,15 @@
+import { useDispatch, useSelector } from "react-redux"
+import { decrement, increment } from "./store/userSlice"
+import { RootState } from "./store/store"
 
 export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  const value: number = useSelector((state: RootState) => state.user.value)
+  const dispatch = useDispatch()
+
+  return <div>
+    <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    <button onClick={() => dispatch(decrement())} className="px-3">-</button>
+    {value}
+    <button onClick={() => dispatch(increment())} className="px-3">+</button>
+  </div>
 }
