a71e9babc4c1006efc112b74ac0e51977260acd4
GalibIbr <galibiavi4@gmail.com>
2024-07-17 00:52:30 +0300

    Cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,22 @@
-import { useDispatch, useSelector } from "react-redux"
-import { decrement, increment } from "./store/userSlice"
-import { RootState } from "./store/store"
-import { useGetUsersQuery } from "./api/userApi"
-import { User } from "./helpers/types"
+import { useGetUsersQuery } from "./api/userApi";
+import { User } from "./helpers/types";
 
 export default function App() {
-  const value: number = useSelector((state: RootState) => state.user.value)
-  const dispatch = useDispatch()
+   const { data, isLoading, error } = useGetUsersQuery();
 
-  const { data, isLoading, error } = useGetUsersQuery()
+   if (error) return <div>Error</div>;
 
-  return (
-     <div>
-        <div className="App">
-           {error ? (
-              <>Oh no, there was an error</>
-           ) : isLoading ? (
-              <>Loading...</>
-           ) : data ? (
-              <>
-              {data.map((user: User) => (
-                 <h3 key={user.id}>{user.fullname}</h3>
-              ))}
-              </>
-           ) : null}
-        </div>
-        <h1 className="text-3xl font-bold underline">Hello world!</h1>
-        <button onClick={() => dispatch(decrement())} className="px-3">
-           -
-        </button>
-        {value}
-        <button onClick={() => dispatch(increment())} className="px-3">
-           +
-        </button>
-     </div>
-  );
+   if (isLoading) return <div>Loading...</div>;
+
+   return (
+      <div>
+         {data ? (
+            <>
+               {data.map((user: User) => (
+                  <h3 key={user.id}>{user.fullname}</h3>
+               ))}
+            </>
+         ) : null}
+      </div>
+   );
 }
