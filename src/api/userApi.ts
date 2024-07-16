35c2d0ffcac0b1b4d23cecca2b70b6e6d8fd0fc4
GalibIbr <galibiavi4@gmail.com>
2024-07-17 00:52:53 +0300

    Add tagTypes

--- src/api/userApi.ts
+++ src/api/userApi.ts
@@ -5,9 +5,11 @@ import { apiUrl } from "../helpers/env";
 export const userApi = createApi({
    reducerPath: 'userApi',
    baseQuery: fetchBaseQuery({ baseUrl: apiUrl }),
+   tagTypes: ['User'],
    endpoints: (builder) => ({
       getUsers: builder.query<User[], void>({
          query: () => 'data',
+         providesTags: ['User']
       })
    })
 })
