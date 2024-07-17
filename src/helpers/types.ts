c8536f180acae15ab54f489bdc827302aa4cfbbc
GalibIbr <galibiavi4@gmail.com>
2024-07-17 12:54:03 +0300

    Update UserI

--- src/helpers/types.ts
+++ src/helpers/types.ts
@@ -1,11 +1,9 @@
 export interface UserI {
    id: number
-   sex: 'male' | 'female'
    fullname: string
    email: string
    phone: string
    age: number
-   languages: string[]
    major: string
    skills: string[]
    experience: 'no experience' | 'less than 1 year' | '1-3 years' | '3-5 years' | '5+ years'
