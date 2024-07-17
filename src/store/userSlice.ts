8f9a1ed0440b490453de59924e36d20a13ff9bc8
GalibIbr <galibiavi4@gmail.com>
2024-07-17 14:28:07 +0300

    Save newUser data from formSubmit

--- src/store/userSlice.ts
+++ src/store/userSlice.ts
@@ -1,26 +1,50 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { formErrors, UserI } from "../helpers/types";
+import type { PayloadAction } from "@reduxjs/toolkit";
 
 export interface UserState {
-   value: number
-} 
-
-const initialState: UserState = {
-   value: 0
+   newUser: UserI;
+   formErrors: formErrors;
 }
 
+const initialState: UserState = {
+   newUser: {
+      id: 0,
+      fullname: "",
+      email: "",
+      phone: "",
+      age: 0,
+      major: "",
+      skills: [],
+      experience: "no experience",
+   },
+   formErrors: {
+      fullname: null,
+      age: null,
+   },
+};
+
 export const userSlice = createSlice({
-   name: 'user',
+   name: "user",
    initialState,
    reducers: {
-      increment: (state) => {
-         state.value += 1
+      submitNewUser: (state, { payload }: PayloadAction<UserI>) => {
+         const { fullname, age } = payload;
+
+         state.formErrors.fullname = fullname.split(" ").length < 2
+            ? "Please enter your full name"
+            : null;
+         state.formErrors.age = age <= 10 || age > 100
+            ? "Please enter a valid age"
+            : null;
+
+         if (fullname.split(" ").length === 2 && age >= 10 && age < 100) {
+            state.newUser = payload;
+         }
       },
-      decrement: (state) => {
-         state.value -= 1
-      }
-   }
-})
+   },
+});
 
-export const { increment, decrement } = userSlice.actions
+export const { submitNewUser } = userSlice.actions;
 
-export default userSlice.reducer
+export default userSlice.reducer;
